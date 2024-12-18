55d082ef6cdda6af147a648641b51a98add7b55f
Nazar Lenyshyn <nazarleny01@gmail.com>
2024-12-18 09:21:19 +0200

    functionality_b version=1.0.0

---
+++ Models/rnn.py
@@ -0,0 +1,5 @@
+''' Module which contains implrmrntation of RNN'''
+
+
+def rnn() -> None:
+    print(f'Implementation of rnn')
---
+++ Preprocessing/eda.py
@@ -0,0 +1,7 @@
+''' Module which contains implementation of all necessary function for eda '''
+
+
+import pandas as pd
+
+def df_wide_info(df: pd.DataFrame) -> None:
+    print(f'Implementation of df_wide_info')
